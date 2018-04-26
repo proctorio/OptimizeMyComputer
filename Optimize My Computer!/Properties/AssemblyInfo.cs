d88c6ad2edbf08027a058b516881be23c3206611
Mike Olsen <mike@proctorio.com>
2018-04-26 06:35:42 -0700

    add "Proctorio" to assembly name per client request, bump version and copyright year

--- Optimize My Computer!/Properties/AssemblyInfo.cs
+++ Optimize My Computer!/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Optimize My Computer!")]
+[assembly: AssemblyTitle("Proctorio Optimize My Computer!")]
 [assembly: AssemblyDescription("cc Mike Olsen, Proctor.io Inc")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Optimize My Computer!")]
-[assembly: AssemblyCopyright("Copyright Proctorio ©  2015")]
+[assembly: AssemblyCopyright("Copyright Proctorio ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
